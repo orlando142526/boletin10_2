bf5adf62266af01e9fb5d2fbefb2069581e8039f
orlando142526 <orlando142526@gmail.com>
2019-12-12 23:04:15 +0100

    first commit

---
+++ boletin10/pkg2/Boletin102.java
@@ -0,0 +1,13 @@
+
+package boletin10.pkg2;
+
+public class Boletin102 {
+
+    public static void main(String[] args) {
+      
+ Adivina2 obx = new Adivina2();
+        obx.pedirNumero1();
+        obx.Adivinar();
+    }
+    
+}
